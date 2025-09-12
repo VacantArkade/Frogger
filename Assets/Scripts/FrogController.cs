f64449d434a42acd407a510b991a3258d75c0eef
VacantArkade <kadehuskey@gmail.com>
2025-09-12 11:21:40 -0500

    Prog

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -4,40 +4,74 @@ using System.Collections;
 
 public class FrogController : MonoBehaviour
 {
+    [SerializeField] Animator anim;
+
+    [SerializeField] float moveTime = 0.15f;
+    [SerializeField] bool canMove;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        canMove = true;
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerUpdate();
+        if (canMove)
+        {
+            PlayerUpdate();
+        }
     }
 
     void PlayerUpdate()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
             PlayerMove(Vector2.up);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
             PlayerMove(Vector2.down);
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
             PlayerMove(Vector2.left);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            transform.rotation = Quaternion.Euler(0, 0, -90);
             PlayerMove(Vector2.right);
         }
     }
 
     void PlayerMove(Vector2 _direction)
     {
-        transform.position += (Vector3)_direction;
+        canMove = false;
+        anim.SetTrigger("Hop");
+        Vector2 _destination = transform.position + (Vector3)_direction;
+        StartCoroutine(LerpMove(_destination));
+        //transform.position += (Vector3)_direction;
+    }
+
+    IEnumerator LerpMove(Vector2 _destination)
+    {
+        Vector2 startPos = transform.position;
+        float elapsedTime = 0f;
+        float duration = 0.1f;
+
+        while (elapsedTime < duration)
+        {
+            float _time = elapsedTime / duration;
+            transform.position = Vector2.Lerp(startPos, _destination, _time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = _destination;
+        yield return new WaitForSeconds(moveTime);
+        canMove = true;
     }
 }
